dcaa92236f78940753fd6fb20be349ef3b322735
rawbenny <rawbenny@gmail.com>
2015-07-03 10:41:05 +0800

    update

--- jh/target/classes/annotations/jh.table.sql
+++
@@ -1,9 +0,0 @@
---Employee Table
-drop table employee;
-
-CREATE TABLE employee (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(50) DEFAULT NULL,
-  PRIMARY KEY (id)
-) ;
-
--- jh/target/classes/jh.schema.sql
+++
@@ -1,4 +0,0 @@
--- CREATE a JH schema
-CREATE SCHEMA JH;
-
--- todo: need to add all the relevant tables here
